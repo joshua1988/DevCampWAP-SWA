ad582f3713ee4f3bdc3cb1a99b8fe67baa898e58
joshua1988 <jangkeehyo@gmail.com>
2017-05-11 15:09:07 +0900

    Changed the link addressed in popup dialog

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById('share').onclick = function() {
   FB.ui({
     method: 'feed',
-    link: 'https://developers.facebook.com/docs/',
+    link: 'https://joshua1988.github.io/DevCampWAP-SWA/',
     caption: 'My Facebook profile application',
   }, function(response){});
 };
