c44dc2f621575488a5b7d76d696441532065c963
joshua1988 <jangkeehyo@gmail.com>
2017-05-11 14:05:25 +0900

    Added the new Facebook profile page with advanced css and layout

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function clickButton() {
-  alert("프로필을 좋아합니다..!");
-}
+document.getElementsByClassName('feature-item-link').onclick = function() {
+  FB.ui({
+    method: 'feed',
+    link: 'https://developers.facebook.com/docs/',
+    caption: 'My Facebook profile application',
+  }, function(response){});
+};
+
+// function clickButton() {
+//   alert("프로필을 좋아합니다..!");
+// }
